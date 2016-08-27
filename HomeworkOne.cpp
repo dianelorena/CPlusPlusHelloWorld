3c737a9485ee7fa844dc03f50e671a23ac88d712
dianelorena <dianelorena@gmail.com>
2016-08-27 16:03:59 -0500

    Added hello world code

---
+++ HomeworkOne.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HomeworkOne
+//
+//  Created by Diane Lorena on 8/27/16.
+//  Copyright © 2016 Diane Lorena. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Bonjour le monde\n";
+    return 0;
+}
