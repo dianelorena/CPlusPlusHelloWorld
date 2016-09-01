896f8043e39ccfe56d14273c53620e55c405c983
dianelorena <dianelorena@gmail.com>
2016-09-01 12:37:37 -0500

    Added code

---
+++ Homework2.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HomeworkOne
+//
+//  Created by Diane Lorena on 8/27/16.
+//  Copyright © 2016 Diane Lorena. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    cout << "Hello " << getenv("USER") << "!\n";
+    return 0;
+}
